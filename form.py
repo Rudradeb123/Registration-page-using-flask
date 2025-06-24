677e5c30b9d8fa31fe4249590185b53c36ebd43a
RUDRADEB DAS <115949210+Rudradeb123@users.noreply.github.com>
2025-06-25 00:35:03 +0530

    Add files via upload

---
+++ form.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+
+class RegistrationForm(FlaskForm):
+    name = StringField("Full Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[
+        DataRequired(),
+        Length(min=6, message="Password must be at least 6 characters.")
+    ])
+    submit = SubmitField("Register")
